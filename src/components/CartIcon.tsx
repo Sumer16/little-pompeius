9a0f8c2e0f3a5b1f7b4bedcfb4689f203bd162b1
Sumer Patel <sumerpatel.dev@gmail.com>
2024-11-01 22:59:57 -0700

    Designed Responsive NavBar & NavBar Menu

---
+++ src/components/CartIcon.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Image from 'next/image';
+
+const CartIcon = () => {
+  return (
+    <div className='flex items-center gap-4 md:gap-2'>
+      <div className='relative w-8 h-8 md:w-5 md:h-5'>
+        <Image src='/cart.png' alt='Cart' fill />
+      </div>
+      <span>Cart (3)</span>
+    </div>
+  )
+}
+
+export default CartIcon;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,50 @@
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import Menu from './Menu';
+import CartIcon from './CartIcon';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='h-12 text-red-500 p-4 flex flex-row items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-16 xl:px-40'>
+      {/* Left Links */}
+      <div className='hidden md:flex gap-4 flex-1'>
+        <Link href='/' className='font-medium'>Home</Link>
+        <Link href='/menu' className='font-medium'>Menu</Link>
+        <Link href='/contact' className='font-medium'>Contact</Link>
+      </div>
+
+      {/* Logo */}
+      <div className='text-xl font-semibold md:font-bold flex-1 md:text-center'>
+        <Link href='/'>Little Pompeius</Link>
+      </div>
+
+      {/* Mobile Menu */}
+      <div className='md:hidden'>
+        <Menu />
+      </div>
+
+      {/* Right Links */}
+      <div className='hidden md:flex gap-4 items-center justify-end flex-1'>
+        <div className='md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-3 rounded-3xl'>
+          <Image src='/phone.png' alt='Phone Number' width={20} height={20} />
+          <span className='font-medium'>555 (8172)</span>
+        </div>
+
+        {
+          true ? (
+            <Link href='/login' className='font-medium'>Login</Link>
+          ) : (
+            <Link href='/orders' className='font-medium'>Orders</Link>
+          )
+        }
+
+        <Link href='/cart'>
+          <CartIcon />
+        </Link>
+      </div>
+    </div>
   )
 }
 
