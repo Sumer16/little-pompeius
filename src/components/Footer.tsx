e68aef7cf2dbb36875b9a5a0cefd3fbde881f7d3
Sumer Patel <sumerpatel.dev@gmail.com>
2024-11-01 23:19:34 -0700

    Designed Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='h-12 md:h-24 p-4 lg:p-16 xl:p-40 text-red-500 flex items-center justify-between'>
+      <Link href='/' className='font-bold text-lg uppercase'>Little Pompeius</Link>
+      <span className='font-mono'>© ALL RIGHTS RESERVED.</span>
+    </div>
   )
 }
 
