c4a588268452aba5bc451bab3f12ff6d1292e7e9
Sumer Patel <sumerpatel.dev@gmail.com>
2024-11-02 17:40:11 -0700

    Organizing code

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -5,7 +5,7 @@ import { menuCategory } from '@/data';
 
 const MenuPage = () => {
   return (
-    <div className='p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center'>
+    <div className='p-4 lg:px-16 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center'>
       {
         menuCategory.map((category) => (
           <Link
