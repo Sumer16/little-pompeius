1dcd8d50fafb2d8e49ec78ac16f61f0946f66d74
Sumer Patel <sumerpatel.dev@gmail.com>
2024-11-02 18:47:33 -0700

    Mobile responsive Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <div className='h-12 md:h-24 p-4 lg:px-16 lg:py-2 xl:px-40 xl:py-10 text-red-500 border-t-2 border-t-red-500 flex items-center justify-between'>
+    <div className='sm:h-12 md:h-24 p-4 lg:px-16 lg:py-2 xl:px-40 xl:py-10 text-red-500 border-t-2 border-t-red-500 flex items-center justify-between'>
       <Link href='/' className='font-bold text-lg uppercase'>Little Pompeius</Link>
-      <span className='font-mono'>© ALL RIGHTS RESERVED.</span>
+      <span className='font-mono text-end'>© ALL RIGHTS RESERVED.</span>
     </div>
   );
 }
